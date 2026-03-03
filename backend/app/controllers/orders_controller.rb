57eb7b70ce02c4d31880ed04f42c11ff81708cb7
Walter Kraxberger <wkrax@hotmail.com>
2026-03-03 17:47:31 -0300

    added create

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -9,6 +9,16 @@ class OrdersController < ApplicationController
     render json: order
   end
 
+  def create
+    order = Order.new(order_params)
+
+    if order.save
+      render json: order, status: :created
+    else
+      render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def order_params
