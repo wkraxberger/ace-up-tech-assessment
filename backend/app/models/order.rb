09e0ce7fce8416d908939726929a518243eebf03
Walter Kraxberger <wkrax@hotmail.com>
2026-03-05 16:26:49 -0300

    calculated the total before saving the order

--- backend/app/models/order.rb
+++ backend/app/models/order.rb
@@ -6,8 +6,13 @@ class Order < ApplicationRecord
   validates :customer_email, presence: true
   validates :status, presence: true
   validate :no_duplicate_products
+  before_save :calculate_total
 
   private
+
+  def calculate_total
+    self.total_price = order_items.sum { |item| item.product.price * item.quantity }
+  end
 
   def no_duplicate_products
     duplicates = order_items.group_by(&:product_id).select { |_, v| v.size > 1 }
