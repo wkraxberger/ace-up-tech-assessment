87d493f30a3441cf05ad56dbe5c8eb8c6931e317
Walter Kraxberger <wkrax@hotmail.com>
2026-03-05 15:04:48 -0300

    set email send to testing as we dont have an email client nor domain

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -33,13 +33,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :test
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
-
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
